bd9be84068ea029c70cb2cdaa0dc78418c692822
Christian Khan <christiankhan@Hotmail.com>
2021-01-09 20:59:09 -0500

    blog component added

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react'; 
-import {useSelector, useDispatch} from 'react-redux'; 
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import { selectUserInput, setBlogData } from '../app/features/userSlice';
+import '../styles/Blogs.css'; 
 
 
 const Blogs = () => {
@@ -28,6 +29,28 @@ const Blogs = () => {
     return (
         <div classname="blog__page">
             <h1 className="blog__page__header">Blogs</h1>
+            {loading ? <h1 className="loading">Loading...</h1> : ""}
+            <div className="blogs">
+                {blogs?.articles?.map(blog => (
+                    <a className="blog" target="_blank" href={blog.url}>
+                        <img src={blog.image} />
+                        <div>
+                            <h3 className="sourceName">
+                                <span>{blog.source.name}</span>
+                                <div></div>
+                                <p>{blog.publishedAt}</p>
+                            </h3>
+                            <h1>{blog.title}</h1>
+                            <p>{blog.description}</p>
+                        </div>
+                    </a>
+                ))}
+                {blogs?.totalArticles == 0 && (
+                    <h1 className="no__blogs">
+                        No news available for you search term.
+                    </h1>
+                )}
+            </div>
 
         </div>
     )
